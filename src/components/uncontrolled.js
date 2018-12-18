fc3502eac05e0a21f327e689d148e707479169a1
umairx97 <umair.bajwa@hotmail.com>
2018-12-18 17:03:25 +0500

    Initial Commit

---
+++ src/components/uncontrolled.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Uncontrolled extends Component {
+
+    
+
+    handleSubmitClick = (event) => { 
+        event.preventDefault();
+
+        const values = { 
+            name: this.name.value,
+            lastname: this.lastname.value
+        }
+
+        console.log(values);
+    }
+
+
+    render(){
+        return(
+            <div className = "container">
+               <form> 
+                   <div className = "form_element"> 
+                        <label> Enter Name</label>
+                        <input type = "text"
+                        ref = {input => this.name = input}
+                        />
+                   </div>
+
+                   <div className = "form_element"> 
+                        <label> Enter Lastname</label>
+                        <input type = "text"
+                         ref = {input => this.lastname = input}
+                         />
+                   </div> 
+                    <button onClick = {this.handleSubmitClick}>Sign in</button>
+               </form>
+            </div>
+        )
+    } 
+}
+
+export default Uncontrolled;
