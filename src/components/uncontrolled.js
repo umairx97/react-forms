3bd816c6c7bb4640b65df07c0055e3aa444df6e1
umairx97 <umair.bajwa@hotmail.com>
2018-12-25 22:42:24 +0500

    Code Refactor

--- src/components/uncontrolled.js
+++ src/components/uncontrolled.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 class Uncontrolled extends Component {
 
-    
-
     handleSubmitClick = (event) => { 
         event.preventDefault();
-
         const values = { 
             name: this.name.value,
             lastname: this.lastname.value
         }
 
         console.log(values);
+
     }
+ 
+    
 
 
     render(){
